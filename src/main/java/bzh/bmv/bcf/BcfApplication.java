8271e09159b11d618db3e386d5763f669622c7e1
ValentinDupitier <valentindupitier@gmail.com>
2018-09-03 14:32:32 +0200

    InitialCommit

---
+++ src/main/java/bzh/bmv/bcf/BcfApplication.java
@@ -0,0 +1,12 @@
+package bzh.bmv.bcf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcfApplication.class, args);
+	}
+}
